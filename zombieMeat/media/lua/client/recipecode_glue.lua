97cdbd4de0fb6ed8353b0c0e5e35ae390c9edbee
SmellyNS <dmitryl1189@gmail.com>
2023-03-06 01:25:00 +0300

    glue added, soap fixed

--- zombieMeat/media/lua/client/recipecode_glue.lua
+++ zombieMeat/media/lua/client/recipecode_glue.lua
@@ -2,7 +2,7 @@ function onCreateMixBoneFlour(items, result, player)
 	local inv = player:getInventory();
 	inv:AddItem("ZombieMeat.MixedBoneFlour");
 end
-function onCreateMixBoneFlour(items, result, player)
+function onCreateExtractGlue(items, result, player)
 	local inv = player:getInventory();
 	inv:AddItem("Pot");
 end
