12a5d3a7ae413692f82d34c3728acfbab05f1402
SmellyNS <dmitryl1189@gmail.com>
2023-03-06 00:15:39 +0300

    glue first try

---
+++ zombieMeat/media/lua/client/recipecode_glue.lua
@@ -0,0 +1,8 @@
+function onCreateMixBoneFlour(items, result, player)
+	local inv = player:getInventory();
+	inv:AddItem("ZombieMeat.MixedBoneFlour");
+end
+function onCreateMixBoneFlour(items, result, player)
+	local inv = player:getInventory();
+	inv:AddItem("Pot");
+end
