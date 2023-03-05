f1fc2879cb42eb8534732b2e7bd5d3c5044671ab
SmellyNS <dmitryl1189@gmail.com>
2023-03-05 17:43:48 +0300

    Bone Knife removed, randomizer for butcher result added, bones and fat added as result of butcher

--- zombieMeat/media/lua/client/recipecode_zm.lua
+++ zombieMeat/media/lua/client/recipecode_zm.lua
@@ -1,5 +1,27 @@
+local function addFinalResult(player, typeOfResult)
+	local inv = player:getInventory();
+	arr = {}
+	for i = 0, 9 do
+		arr[i] = math.random(10)
+	end
+	table.sort(arr)
+	local actualResult = arr[player:getPerkLevel(Perks.Cooking)]
+	for i = 0, actualResult do
+		inv:AddItem(typeOfResult);
+	end
+end
+
 function Butcher(items, result, player)
 	local inv = player:getInventory();
-	inv:AddItem("ZombieMeat.HumanMeat");
-	inv:AddItem("ZombieMeat.HumanMeat");
+	
+	addFinalResult(player, "ZombieMeat.HumanMeat")
+	addFinalResult(player, "ZombieMeat.ZMFat")
+	addFinalResult(player, "ZombieMeat.RottenBone")
+	
 end
+
+
+
+
+
+player:getPerkLevel(Perks.MetalWelding)
