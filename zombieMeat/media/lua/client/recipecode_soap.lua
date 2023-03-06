e4bcc8423309ed3004995c6f7c7b68cf9e3d9f82
AndrewPatrin <115947375+AndrewPatrin@users.noreply.github.com>
2023-03-07 00:46:56 +0400

    adding candles

--- zombieMeat/media/lua/client/recipecode_soap.lua
+++ zombieMeat/media/lua/client/recipecode_soap.lua
@@ -6,3 +6,21 @@ function ExtractSoapFromThePot(items, result, player)
 	local inv = player:getInventory();
 	inv:AddItem("Pot");
 end
+function CreatePreparedFat(items, result, player)
+    local inv = player:getInventory();
+	inv:AddItem("ZombieMeat.ZMPreparedFat");
+end
+function CreateBilletCandles(items, result, player)
+    local inv = player:getInventory();
+	inv:AddItem("ZombieMeat.ZMBilletCandles");
+end
+function ExtractCandlesFromTheTray(items, result, player)
+	local inv = player:getInventory();
+	inv:AddItem("MuffinTray");
+end
+function OnTestIsHeat(item)
+    if instanceof(item, "Food") then
+    return not item:GetHeat < 1.3 and item:IsCooked()
+    end
+    return true
+end
